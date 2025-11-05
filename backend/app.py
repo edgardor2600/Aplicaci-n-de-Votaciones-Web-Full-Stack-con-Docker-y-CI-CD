41b40a328226d1b4724c6a75cc295e630f13d375
Edgardo Rodriguez <edgardorodriguez@uts.edu.co>
2025-11-04 20:37:38 -0500

    fix:" add new requirements"

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,10 @@ import time
 import psycopg2
 import click
 from flask import Flask, jsonify, g
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "votes")
